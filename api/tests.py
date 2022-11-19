1d0b8b1ad42d3f41cb80f0b89f0f6b65e27c6675
Временный пользователь <U.temp@DNTTM.NET>
2022-11-19 20:04:59 +0300

    Добавлены тесты для метода POST

--- api/tests.py
+++ api/tests.py
@@ -1,16 +1,8 @@
 from rest_framework.test import APITestCase
 
+
 class StatisticsTests(APITestCase):
     def test_api(self):
-        # url = reverse('statistics', args='abc')
         url = 'https://127.0.0.1:8000/api/abc'
         response = self.client.get(url)
-        self.assertEqual(response.data,{
-            "symbol_count": {
-                "a": 1,
-                "b": 1,
-                "c": 1
-            },
-            "str_len": 3
-        })
-
+        self.assertEqual(response.data, {"symbol_count": {"a": 1, "b": 1, "c": 1}, "str_len": 3})
